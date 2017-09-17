c9d2caf2ad040e1ec8e56d095076802a62e5aeb7
Renaud Chaput <renchap@gmail.com>
2017-09-17 22:00:34 +0200

    Add collapsing synopsis

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,16 @@
-require('../styles/index')
+require('../styles/index');
 
 if (process.env.NODE_ENV != "production") {
-  require('../index.html')
+  require('../index.html');
 }
+
+import $ from 'jquery';
+
+$().ready(function() {
+  $('.synopsis').each(function(i, e) {
+    var el = $(e);
+    el.on('click', function() {
+      $(this).toggleClass("show");
+    })
+  });
+})
