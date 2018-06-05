d755833f8293f76242a0e4a24c7a51bccf677819
Renaud Chaput <renchap@gmail.com>
2018-06-05 23:03:18 +0200

    Upgrade all deps, including Webpack to 4, run Prettier

--- javascript/index.js
+++ javascript/index.js
@@ -1,21 +1,21 @@
-require('../styles/index');
+require("../styles/index")
 
 if (process.env.NODE_ENV != "production") {
-  require('../index.html');
+  require("../index.html")
 }
 
-import $ from 'jquery';
+import $ from "jquery"
 
 $().ready(function() {
-  $('.synopsis a').each(function(i, el) {
+  $(".synopsis a").each(function(i, el) {
     $(el).click(function(e) {
-      e.stopPropagation();
-    });
-  });
+      e.stopPropagation()
+    })
+  })
 
-  $('.synopsis').each(function(i, el) {
+  $(".synopsis").each(function(i, el) {
     $(el).click(function(e) {
-      $(this).toggleClass("show");
-    });
-  });
+      $(this).toggleClass("show")
+    })
+  })
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,78 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
-const webpack = require('webpack')
-const path = require('path')
-const { env } = require('process')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CleanWebpackPlugin = require("clean-webpack-plugin")
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin")
+const webpack = require("webpack")
+const path = require("path")
+const { env } = require("process")
 
-const isProduction = env.NODE_ENV === 'production'
-
-const loaders = {
-  css: {
-    loader: 'css-loader',
-    options: { minimize: isProduction }
-  },
-  postcss: {
-    loader: 'postcss-loader',
-  },
-  sass: {
-    loader: 'sass-loader',
-  }
-}
-
-var styleLoader = [];
-
-if(isProduction) {
-  styleLoader = ExtractTextPlugin.extract({
-    fallback: 'style-loader',
-    use: [loaders.css, loaders.postcss, loaders.sass]
-  })
-} else {
-  styleLoader = ['style-loader', loaders.css, loaders.postcss, loaders.sass]
-}
+const isProduction = env.NODE_ENV === "production"
 
 const config = {
+  mode: isProduction ? "production" : "development",
   entry: {
-    app: ['./javascript/index.js']
+    app: ["./javascript/index.js"],
   },
 
   devServer: {
     contentBase: false,
-    hot: !isProduction
+    hot: !isProduction,
   },
 
   module: {
     rules: [
       {
         test: /\.(sass|scss|css)$/,
-        use: styleLoader,
+        use: [
+          !isProduction ? "style-loader" : MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { minimize: isProduction },
+          },
+          "postcss-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.html$/,
-        use: ['html-loader'],
+        use: ["html-loader"],
       },
       {
         test: /\.(jpg|jpeg|png|gif|svg|eot|ttf|woff|woff2)$/i,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: '[name]-[hash].[ext]'
-          }
-        }]
-      }
-    ]
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name]-[hash].[ext]",
+            },
+          },
+        ],
+      },
+    ],
   },
 
   output: {
-    filename: isProduction ? '[name]-[hash].js' : '[name].js',
-    path: path.join(__dirname, isProduction ? 'build-production' : 'build'),
+    filename: isProduction ? "[name]-[hash].js" : "[name].js",
+    path: path.join(__dirname, isProduction ? "build-production" : "build"),
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'index.html',
+      template: "index.html",
     }),
-    new webpack.EnvironmentPlugin({ 'NODE_ENV': 'development' })
+    new webpack.EnvironmentPlugin({ NODE_ENV: "development" }),
   ],
 
   resolve: {
-    extensions: ['.scss', '.css', '.js'],
-    modules: ['node_modules']
-  }
+    extensions: [".scss", ".css", ".js"],
+    modules: ["node_modules"],
+  },
 }
 
 if (isProduction) {
   config.plugins.push(
-    new ExtractTextPlugin('[name]-[contenthash].css'),
-    new CleanWebpackPlugin(['build-production']),
+    new ExtractTextPlugin("[name]-[contenthash].css"),
+    new CleanWebpackPlugin(["build-production"]),
     new UglifyJSPlugin({ sourceMap: false }),
   )
 }
